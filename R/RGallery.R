98739bb6e256b5be7ca9ccf1ad1ae1d246940b63
xinchoubiology <xinchoubiology@gmail.com>
2015-02-18 17:43:17 -0500

    Data simulation for logistic regression init

---
+++ R/RGallery.R
@@ -0,0 +1,6 @@
+##' Logistical regression data simulation
+##' 
+##' @title sim_dat_LR
+##' @aliases LR_simulate
+##' @description A data simulation function which is used for glmnet benchmark. The data set size for L2-regularized 
+##' 
